89ad4b5e0642f3d4dfb8498e92b554af68f38d3f
John Scarrott <johnps@outlook.com>
2016-06-12 22:01:00 +0100

    Changed solving to use recusion

--- ConsoleApplication1/Program.fs
+++ ConsoleApplication1/Program.fs
@@ -13,7 +13,7 @@ module Solver =
       | h::t -> h::remove_first pred t
       | _ -> []
 
-    type Cell(row : int, column: int, possValues : List<int>) =
+    type Cell(row : int, column: int, possValues : List<int>) as self =
 
         let roundUpForGrouping (x:float) =         
             System.Math.Ceiling x
@@ -22,19 +22,36 @@ module Solver =
 
         member this.Column = column
 
+        member this.ID = this.Column.ToString() + this.Row.ToString()
+
         member this.RowGroup = int (roundUpForGrouping( float(this.Row) / 3.0))
 
         member this.ColumnGroup = int(roundUpForGrouping ( float(this.Column) / 3.0))
 
-        member val PossibleValues = possValues with get, set
+        member this.PossibleValues = possValues
 
         member this.Solved = this.PossibleValues.Length = 1
+
+        member this.IsSame (otherCell:Cell) = ((otherCell.Column = self.Column) && (otherCell.Row = self.Row))
+
+        member this.IsDifferent (otherCell:Cell) = not (self.IsSame otherCell)
+
+        member this.checkConflicts (otherCell:Cell) = ((otherCell.Column = self.Column) || (otherCell.Row = self.Row) || ((otherCell.RowGroup = self.RowGroup) && (otherCell.ColumnGroup = self.ColumnGroup))) && (self.PossibleValues |> List.exists (fun x -> x = otherCell.PossibleValues.[0]))
          
 
     let testCell = Cell(5, 8, [1;2;3;4])
 
-    type Grid(numberOfRows : int, numberOfColumns : int,cells : List<Cell>) =
+    type Grid(numberOfRows : int, numberOfColumns : int,cells : List<Cell>, level : int) as self =
 
+        member this.solvedCells = 
+            self.Cells |> List.filter (fun (x:Cell) -> x.Solved)
+
+        member this.unsolvedCells =
+            self.Cells |> List.filter (fun (x:Cell) -> not x.Solved)
+
+        member this.isFinished = this.unsolvedCells.Length = 0
+
+        member this.Level = level
 
         member this.Rows = [1 .. numberOfRows]
 
@@ -48,38 +65,35 @@ module Solver =
 
             else cells
 
-        
+
+
+    let rec cellListToMap (list:List<Cell>, cellMap:Map<string, Cell>) =
+        if(list.IsEmpty) then cellMap
+        else 
+            let appendedCellMap = cellMap.Add(list.Head.ID, list.Head)
+            cellListToMap(list.Tail, appendedCellMap)
 
     let printGrid (grid:Grid) =
-        for i = 0 to 80 do
-            if(grid.Cells.[i].Solved) then
-                printf "%i " grid.Cells.[i].PossibleValues.[0]
-            else printf "0 "
-            if( (i+1) % 9 = 0) then printfn ""
-
-            
-    let printRowGroups (grid:Grid) =
-        for i = 0 to 80 do
-            printf "%i " grid.Cells.[i].RowGroup
-            if( (i+1) % 9 = 0) then printfn ""
-
-
-
-    let printColumnGroups (grid:Grid) =
-        for i = 0 to 80 do
-            printf "%i " grid.Cells.[i].ColumnGroup
-            if( (i+1) % 9 = 0) then printfn ""
-
-    let printColumn (grid:Grid) =
-        for i = 0 to 80 do
-            printf "%i " grid.Cells.[i].Column
-            if( (i+1) % 9 = 0) then printfn ""
+        printf "Grid: "
+        printfn ""
+        let cellMap = cellListToMap(grid.Cells, Map.empty)
+        for i = 1 to 9 do
+            for j = 1 to 9 do
+                let ID = i.ToString() + j.ToString()
+                let cell = cellMap.[ID]
+                if(cell.Solved) then 
+                    printf "%i " cell.PossibleValues.[0]
+                else printf "0 "
+            printfn ""
 
 
-    let printRow (grid:Grid) =
-        for i = 0 to 80 do
-            printf "%i " grid.Cells.[i].Row
-            if( (i+1) % 9 = 0) then printfn ""
+    let printCell (cell:Cell) =
+        printfn"Row %i Group %i , Column %i, Group %i, Possible Values %A" cell.Row cell.RowGroup cell.Column cell.ColumnGroup cell.PossibleValues
+
+    let printGrids (grids:List<Grid>) = 
+        printfn "Printing Grids"
+        for grid in grids
+            do printGrid grid
 
                 
                 
@@ -101,24 +115,44 @@ module Solver =
     let startingGrid = 
        Grid(parseCells.Length, parseCells.[0].Length, [for i = 0 to parseCells.Length - 1 do
                                                         for j = 0 to parseCells.[0].Length - 1 do
-                                                            yield Cell(i + 1,j + 1, parseCellValue parseCells.[i].[j])])
+                                                            yield Cell(i + 1,j + 1, parseCellValue parseCells.[i].[j])], 1)
 
-    let solvedCells (grid : Grid) = 
-        grid.Cells |> List.filter (fun x -> x.Solved)
+    
 
-    let unsolvedCells (grid: Grid) =
-        grid.Cells |> List.filter (fun x -> not x.Solved)
+//            printfn "this cell"
+//            printCell cell
+//            printfn "Compared to"
+//            printCell cells.Head
 
-    let isFinished grid = (unsolvedCells grid).Length > 0
+    let rec trySolveValues (cell:Cell, cells:List<Cell> ) =
+        if(cells.IsEmpty) then cell
+        else
+            if(cell.checkConflicts cells.Head)
+            then trySolveValues(Cell(cell.Row, cell.Column, cell.PossibleValues |> List.filter (fun x -> x <> cells.Head.PossibleValues.[0])), cells.Tail)
+            else trySolveValues(cell, cells.Tail)
+    
+    let rec solvePassOnAllCells (cells:List<Cell>, solvedCells:List<Cell>, checkedCells:List<Cell>) =
+        if(cells.IsEmpty)
+         then checkedCells |> List.append solvedCells
+        else 
+            let appendedCheckedCells = trySolveValues(cells.Head, solvedCells |> List.filter(fun x -> cells.Head.IsDifferent x)) :: checkedCells
+            solvePassOnAllCells(cells.Tail, solvedCells, appendedCheckedCells)
 
-    let checkCellPossibleValues (cell:Cell, grid:Grid ) =
-        for gridCell in solvedCells grid do
-            if(((gridCell.Column = cell.Column) || (gridCell.Row = cell.Row) || ((gridCell.RowGroup = cell.RowGroup)
-                && (gridCell.ColumnGroup = cell.ColumnGroup))) && (cell.PossibleValues |> List.exists (fun x -> x = gridCell.PossibleValues.[0]))) 
-                 then
-//                 printfn "Row %i, Column %i removing value %i" cell.Row cell.Column gridCell.PossibleValues.[0]
-                 cell.PossibleValues <- remove_first (fun x -> x = gridCell.PossibleValues.[0]) cell.PossibleValues 
-        
+    let rec checkAllGrids (uncheckedGrids:List<Grid>, checkedGrids:List<Grid>) =
+        if(uncheckedGrids.IsEmpty) then checkedGrids 
+        else
+            let appendedCheckedGrids = Grid(9 ,9, solvePassOnAllCells(uncheckedGrids.Head.unsolvedCells, uncheckedGrids.Head.solvedCells, []), uncheckedGrids.Head.Level + 1) :: checkedGrids
+            checkAllGrids(uncheckedGrids.Tail, appendedCheckedGrids)
+
+    let rec solvePuzzle input:List<Grid> =
+        printGrids input
+        let finishedGrids = (input |> List.filter(fun (x:Grid) -> x.isFinished))
+        let solved = finishedGrids.Length > 0
+        if(solved) then finishedGrids
+        else
+            let appendedGrids = input |> List.append(checkAllGrids(input, []))
+            solvePuzzle(appendedGrids)
+            
                 
 
 
@@ -126,39 +160,49 @@ module Solver =
     printfn ""
     printGrid startingGrid
 
-    printfn "Rows:"
-    printfn ""
-    printRow startingGrid
+//    printfn "Rows:"
+//    printfn ""
+//    printRow startingGrid
+//
+//    printfn "Columns:"
+//    printfn ""
+//    printColumn startingGrid
+//
+//    printfn "Column Groups:"
+//    printfn ""
+//    printColumnGroups startingGrid
+//
+//    printfn "Row Groups:"
+//    printfn ""
+//    printRowGroups startingGrid
 
-    printfn "Columns:"
-    printfn ""
-    printColumn startingGrid
-
-    printfn "Column Groups:"
-    printfn ""
-    printColumnGroups startingGrid
-
-    printfn "Row Groups:"
-    printfn ""
-    printRowGroups startingGrid
-
-
-    while isFinished startingGrid do
-    
-        printfn "Number of solved cells %i" (solvedCells startingGrid).Length
-
+//Simple solve method for a sudoku no support for when there are logical guesses outside the simple solving framework
+//    while isFinished startingGrid do
+//    
+//        printfn "Number of solved cells %i" (solvedCells startingGrid).Length
+//
 //        printGrid startingGrid
-    
-        for cell in unsolvedCells startingGrid do
-            checkCellPossibleValues (cell, startingGrid)
+//    
+//        for cell in unsolvedCells startingGrid do
+//            checkCellPossibleValues (cell, startingGrid)
 
-    printGrid startingGrid
+    let grids = startingGrid :: []
+
+    
+    let finishedGrids = solvePuzzle grids
+
+    for grid in finishedGrids
+        do printGrid grid
+
+//    printGrid startingGrid
 
     let endTime = System.DateTime.Now
 
     let span = endTime.Subtract(start).ToString()
 
     printf "%s" span
+
+    System.Console.Read() |> ignore
 
 [<EntryPoint>]
 let main argv = 
